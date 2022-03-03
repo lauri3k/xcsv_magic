4cc0e7e29a571fe72002da1cb070e11a24f964c7
Lauri Koikkalainen <lauri.koikkalainen@ntnu.no>
2022-03-03 08:08:44 +0100

    test

--- xcsv_magic/server_extensions/formgrader/formgrader.py
+++ xcsv_magic/server_extensions/formgrader/formgrader.py
@@ -9,34 +9,23 @@ from e2xgrader.server_extensions.formgrader import handlers, apihandlers
 from tornado import web
 import os
 
-from e2xgrader.exporters import E2xExporter
-from e2xgrader.preprocessors import FilterCellsById
+from nbconvert.exporters import HTMLExporter
+from nbgrader.apps import NbGrader
 
-from . import handlers as custom_handlers
+from . import handlers
 
 
-class FormgradeExtension(E2xFormgrade):
+class ExportGradesExtension(NbGrader):
     def build_extra_config(self):
-        extra_config = super(E2xFormgrade, self).build_extra_config()
-        extra_config.E2xExporter.template_file = "formgrade"
-        extra_config.E2xExporter.template_path = [
-            handlers.template_path,
-            custom_handlers.template_path,
-            nbgrader_handlers.template_path,
-        ]
-        self.log.info(f"Template paths: {extra_config.E2xExporter.template_path}")
+        extra_config = super(ExportGradesExtension, self).build_extra_config()
+        extra_config.HTMLExporter.template_file = "export_grades"
+        extra_config.HTMLExporter.template_path = [handlers.template_path]
         return extra_config
 
     def init_tornado_settings(self, webapp):
         # Init jinja environment
         jinja_env = Environment(
-            loader=FileSystemLoader(
-                [
-                    handlers.template_path,
-                    custom_handlers.template_path,
-                    nbgrader_handlers.template_path,
-                ]
-            ),
+            loader=FileSystemLoader([handlers.template_path]),
             autoescape=True,
         )
 
@@ -55,9 +44,6 @@ class FormgradeExtension(E2xFormgrade):
         else:
             nbgrader_bad_setup = False
 
-        exporter = E2xExporter()
-        exporter.register_preprocessor(FilterCellsById)
-
         # Configure the formgrader settings
         tornado_settings = dict(
             nbgrader_url_prefix=os.path.relpath(
@@ -65,7 +51,7 @@ class FormgradeExtension(E2xFormgrade):
             ),
             nbgrader_coursedir=self.coursedir,
             nbgrader_authenticator=self.authenticator,
-            nbgrader_exporter=exporter,
+            nbgrader_exporter=HTMLExporter(config=self.config),
             nbgrader_gradebook=None,
             nbgrader_db_url=self.coursedir.db_url,
             nbgrader_jinja2_env=jinja_env,
@@ -77,26 +63,12 @@ class FormgradeExtension(E2xFormgrade):
     def init_handlers(self, webapp):
         h = []
         h.extend(handlers.default_handlers)
-        h.extend(custom_handlers.default_handlers)
-        h.extend(apihandlers.default_handlers)
-        h.extend(nbgrader_handlers.default_handlers)
-        h.extend(nbgrader_apihandlers.default_handlers)
         h.extend(
             [
                 (
-                    r"/formgrader/static/(.*)",
-                    web.StaticFileHandler,
-                    {"path": nbgrader_handlers.static_path},
-                ),
-                (
-                    r"/e2xgrader/static/(.*)",
-                    web.StaticFileHandler,
-                    {"path": handlers.static_path},
-                ),
-                (
                     r"/csv_magic/static/(.*)",
                     web.StaticFileHandler,
-                    {"path": custom_handlers.static_path},
+                    {"path": handlers.static_path},
                 ),
                 (r"/formgrader/.*", nbgrader_handlers.Template404),
             ]
@@ -114,7 +86,7 @@ def load_jupyter_server_extension(nbapp):
     """Load the fancypants extension"""
     nbapp.log.info("Loading the xcsv_magic serverextension")
     webapp = nbapp.web_app
-    formgrader = FormgradeExtension(parent=nbapp)
+    formgrader = ExportGradesExtension(parent=nbapp)
     formgrader.log = nbapp.log
     formgrader.initialize([])
     formgrader.init_tornado_settings(webapp)
